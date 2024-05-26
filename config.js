c59a5b99570434f22c29cd0927323e781affc512
Kyli Brook Johnson <kylibjohnson@gmail.com>
2024-05-26 15:22:10 -0500

    GET Test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-4c770439-44a2-4b16-8b49-b5e48a99af26.containerhub.tripleten-services.com"
 }
--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('status code should be 200', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		actualStatusCode = response.status;
+		console.log(actualStatusCode);
 	} catch (error) {
 		console.error(error);
 	}
+	expect (actualStatusCode).toBe(200)
 });
+
