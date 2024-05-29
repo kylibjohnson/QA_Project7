ffcbf577832d4388ed790058e631770f4c8675c2
Kyli Brook Johnson <kylibjohnson@gmail.com>
2024-05-29 17:55:25 -0500

    update URL 2

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-8f629766-0e56-4bb0-872e-314ad4f1a37e.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-b7d71526-4044-4c65-b4bb-512191d5d63c.containerhub.tripleten-services.com"
 }
